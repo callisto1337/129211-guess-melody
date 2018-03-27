262dbc64dfcf910490753524ec5ad567e4bc736f
callisto1337 <hazardous333@gmail.com>
2018-03-28 01:24:45 +0300

    добавил переключение страниц и указал ссылку на наставника

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const template = document.querySelector('section.main'),
+    pages = [
+    'main--welcome',
+    'main--level-genre',
+    'main--level-artist',
+    'main--result'
+];
+
+
+document.querySelector('section.main').webkitRequestFullScreen()
